f32fb1eb2dbb619089ad9efc9ceb4c6e4d5a6d34
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-02-17 20:25:13 +0000

    trial write test

--- bert_models.py
+++ bert_models.py
@@ -93,130 +93,126 @@ def fine_tuning_bert_re_ranker(model, train_dataloader, validation_dataloader, e
     for epoch_i in range(0, epochs):
 
         metrics = []
-        #
-        # # ========================================
-        # #               Training
-        # # ========================================
-        #
-        # print("")
-        # print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
-        # print('Training...')
-        #
-        # t0 = time.time()
-        #
-        # total_loss = 0
-        #
-        # model.train()
-        #
-        # for step, batch in enumerate(train_dataloader):
-        #
-        #     # Progress update every 40 batches.
-        #     if step % 40 == 0 and not step == 0:
-        #         # Calculate elapsed time in minutes.
-        #         elapsed = format_time(time.time() - t0)
-        #
-        #         # Report progress.
-        #         print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
-        #
-        #     b_input_ids = batch[0].to(device)
-        #     b_token_type_ids = batch[1].to(device)
-        #     b_attention_mask = batch[2].to(device)
-        #     b_labels = batch[3].to(device, dtype=torch.float)
-        #
-        #     model.zero_grad()
-        #
-        #     outputs = model(input_ids=b_input_ids, attention_mask=b_attention_mask, token_type_ids=b_token_type_ids,
-        #                     labels=b_labels)
-        #
-        #     loss = outputs[0]
-        #
-        #     total_loss += loss.item()
-        #
-        #     loss.backward()
-        #     torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        #
-        #     optimizer.step()
-        #
-        #     # Update the learning rate.
-        #     scheduler.step()
-        #
-        # # Calculate the average loss over the training data.
-        # avg_train_loss = total_loss / len(train_dataloader)
-        # print(avg_train_loss)
-        # metrics.append('Average training loss: ' + str(avg_train_loss) + '\n')
-        #
-        # # Store the loss value for plotting the learning curve.
-        # loss_values.append(avg_train_loss)
-        #
-        # print("")
-        # print("  Average training loss: {0:.5f}".format(avg_train_loss))
-        # print("  Training epoch took: {:}".format(format_time(time.time() - t0)))
-        #
-        # # ========================================
-        # #               Validation
-        # # ========================================
-        # if do_eval:
-        #
-        #     print("---")
-        #     print('Validation...')
-        #
-        #     t0 = time.time()
-        #
-        #     model.eval()
-        #     eval_loss = 0
-        #     #eval_accuracy = 0
-        #     nb_eval_steps = 0
-        #     #nb_eval_examples = 0, 0
-        #
-        #     for batch in validation_dataloader:
-        #
-        #         b_input_ids = batch[0].to(device)
-        #         b_token_type_ids = batch[1].to(device)
-        #         b_attention_mask = batch[2].to(device)
-        #         b_labels = batch[3].to(device, dtype=torch.float)
-        #
-        #         with torch.no_grad():
-        #             outputs = model(input_ids=b_input_ids, token_type_ids=b_token_type_ids, attention_mask=b_attention_mask,
-        #                             labels=b_labels)
-        #
-        #         loss = outputs[0]
-        #         #print('*** LOSS ***')
-        #         eval_loss += loss
-        #
-        #         #print('*** PRED ***')
-        #         #pred = outputs[1].numpy().tolist()
-        #         #print(pred)
-        #
-        #         #print('*** GT ***')
-        #         #gt = b_labels.numpy().tolist()
-        #         #print(gt)
-        #
-        #         # Calculate the accuracy for this batch of test sentences.
-        #         # tmp_eval_accuracy = flat_accuracy(logits, label_ids)
-        #
-        #         # Accumulate the total accuracy.
-        #         # eval_accuracy += tmp_eval_accuracy
-        #
-        #         # Track the number of batches
-        #         nb_eval_steps += 1
-        #
-        #     # Report the final accuracy for this validation run.
-        #     avg_validation_loss = eval_loss / nb_eval_steps
-        #     print("")
-        #     print("  Average validation loss: {0:.5f}".format(avg_validation_loss))
-        #     print("  Validation took: {:}".format(format_time(time.time() - t0)))
-        #
-        #     metrics.append('Average validation loss: ' + str(avg_validation_loss) + '\n')
-        #
-        #
-        # else:
-        #     print('*** skipping validation ***')
 
-        # Writing model
-        #TODO - remove
-        metrics.append(['I am a test 1 \n'])
-        metrics.append(['I am a test 2 \n'])
+        # ========================================
+        #               Training
+        # ========================================
 
+        print("")
+        print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+        print('Training...')
+
+        t0 = time.time()
+
+        total_loss = 0
+
+        model.train()
+
+        for step, batch in enumerate(train_dataloader):
+
+            # Progress update every 40 batches.
+            if step % 40 == 0 and not step == 0:
+                # Calculate elapsed time in minutes.
+                elapsed = format_time(time.time() - t0)
+
+                # Report progress.
+                print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
+
+            b_input_ids = batch[0].to(device)
+            b_token_type_ids = batch[1].to(device)
+            b_attention_mask = batch[2].to(device)
+            b_labels = batch[3].to(device, dtype=torch.float)
+
+            model.zero_grad()
+
+            outputs = model(input_ids=b_input_ids, attention_mask=b_attention_mask, token_type_ids=b_token_type_ids,
+                            labels=b_labels)
+
+            loss = outputs[0]
+
+            total_loss += loss.item()
+
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            optimizer.step()
+
+            # Update the learning rate.
+            scheduler.step()
+
+        # Calculate the average loss over the training data.
+        avg_train_loss = total_loss / len(train_dataloader)
+        print(avg_train_loss)
+        metrics.append('Average training loss: ' + str(avg_train_loss) + '\n')
+
+        # Store the loss value for plotting the learning curve.
+        loss_values.append(avg_train_loss)
+
+        print("")
+        print("  Average training loss: {0:.5f}".format(avg_train_loss))
+        print("  Training epoch took: {:}".format(format_time(time.time() - t0)))
+
+        # ========================================
+        #               Validation
+        # ========================================
+        if do_eval:
+
+            print("---")
+            print('Validation...')
+
+            t0 = time.time()
+
+            model.eval()
+            eval_loss = 0
+            #eval_accuracy = 0
+            nb_eval_steps = 0
+            #nb_eval_examples = 0, 0
+
+            for batch in validation_dataloader:
+
+                b_input_ids = batch[0].to(device)
+                b_token_type_ids = batch[1].to(device)
+                b_attention_mask = batch[2].to(device)
+                b_labels = batch[3].to(device, dtype=torch.float)
+
+                with torch.no_grad():
+                    outputs = model(input_ids=b_input_ids, token_type_ids=b_token_type_ids, attention_mask=b_attention_mask,
+                                    labels=b_labels)
+
+                loss = outputs[0]
+                #print('*** LOSS ***')
+                eval_loss += loss
+
+                #print('*** PRED ***')
+                #pred = outputs[1].numpy().tolist()
+                #print(pred)
+
+                #print('*** GT ***')
+                #gt = b_labels.numpy().tolist()
+                #print(gt)
+
+                # Calculate the accuracy for this batch of test sentences.
+                # tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+                # Accumulate the total accuracy.
+                # eval_accuracy += tmp_eval_accuracy
+
+                # Track the number of batches
+                nb_eval_steps += 1
+
+            # Report the final accuracy for this validation run.
+            avg_validation_loss = eval_loss / nb_eval_steps
+            print("")
+            print("  Average validation loss: {0:.5f}".format(avg_validation_loss))
+            print("  Validation took: {:}".format(format_time(time.time() - t0)))
+
+            metrics.append('Average validation loss: ' + str(avg_validation_loss) + '\n')
+
+
+        else:
+            print('*** skipping validation ***')
+
+        # Writing model & metrics
         if write:
             print('Writing model to file')
             if os.path.isdir(model_path):
@@ -336,7 +332,7 @@ if __name__ == "__main__":
     seed_val = 42
     write = True
     model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'test_write_1'
+    experiment_name = 'exp_test_1'
     fine_tuning_bert_re_ranker(model=relevance_bert, train_dataloader=train_dataloader,
                                validation_dataloader=validation_dataloader, epochs=epochs, lr=lr, eps=eps,
                                seed_val=seed_val, write=write, model_path=model_path, experiment_name=experiment_name)
