608d4280e10f85195ebc8870e3c96f9bb3654143
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-02-19 09:25:52 +0000

    ready to run convert larger train dataset

--- convert_training_to_pt.py
+++ convert_training_to_pt.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
 
     output_path = '/nfs/trec_car/data/bert_reranker_datasets/train_1000000+_dataset_from_pickle_v2'
     data_dir = '/nfs/trec_car/data/bert_reranker_datasets/'
-    set_name = 'train_1000000+_'
+    set_name = 'train_1000000+'
 
     convert_dataset_to_pt(set_name, data_path=data_dir, output_path=output_path)
