2208237810925964814730e5ddc2ef1a2e8a4ac4
iain-mackie <i.mackie@me.com>
2020-02-24 13:15:01 +0000

    test sqlitedict

--- trec_car_preprocessing.py
+++ trec_car_preprocessing.py
@@ -200,7 +200,7 @@ def load_corpus(read_path, write_path):
                         else elem.anchor_text
                         for elem in p.bodies]
             mydict[p.para_id] = ' '.join(para_txt)
-            print(p.para_id)
+            print(str(p.para_id))
             print(' '.join(para_txt))
             if i % 1000000 == 0:
                 print('Loading paragraph {} of {}'.format(i, APPROX_TOTAL_PARAGRAPHS))
