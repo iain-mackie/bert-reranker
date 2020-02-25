3fc223fdfca4fc7720dfd7c06878c788f5194186
iain-mackie <i.mackie@me.com>
2020-02-25 13:18:07 +0000

    preprocess validation

--- bert_utils.py
+++ bert_utils.py
@@ -152,7 +152,7 @@ if __name__ == '__main__':
 
     set_name = 'train_benchmarkY1'
     data_path = '/nfs/trec_car/data/bert_reranker_datasets/'
-    output_path = '/nfs/trec_car/data/bert_reranker_datasets/train_benchmarkY1_None.pt'
+    output_path = '/nfs/trec_car/data/bert_reranker_datasets/train_benchmarkY1.pt'
     percent_train = None
     convert_training_dataset_to_pt(set_name=set_name, data_path=data_path, output_path=output_path, percent_train=percent_train)
 
