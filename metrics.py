4f60bd7b48a369b7cb535c8251a1d1feceba3f02
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-02-25 22:14:59 +0000

    changed map cal

--- metrics.py
+++ metrics.py
@@ -1,4 +1,6 @@
 
+import collections
+
 def get_map(run):
 
     correct_docs_sum = sum(run)
@@ -34,55 +36,165 @@ def get_bert_labels(labels, scores):
     return bert_labels
 
 
+def get_stats(labels_groups, scores_groups):
 
-def get_stats(labels, scores):
-
-    bert_labels = get_bert_labels(labels=labels, scores=scores)
-
-    map_labels = get_map(run=labels)
-    map_bert_labels = get_map(run=bert_labels)
-
-    return map_labels, map_bert_labels
-
-
-def split_bert_outputs(label_list, score_list, query_docids_map):
-
-    last_query = ''
-    doc_counter = 0
-    labels = []
-    scores = []
-    query_counter = 0
     map_labels_sum = 0
-    map_bert_labels_sum = 0
+    map_bert_sum = 0
+
+    for i in zip(labels_groups, scores_groups):
+        bert_labels = get_bert_labels(labels=i[0], scores=i[1])
+
+        map_labels_sum += get_map(run=i[0])
+        map_bert_sum += get_map(run=bert_labels)
+
+    map_labels = map_labels_sum / len(labels_groups)
+    map_bert = map_bert_sum / len(labels_groups)
+
+    return map_labels, map_bert
+
+
+def group_bert_outputs_by_query(label_list, score_list, query_docids_map):
+
+    last_query = 'Not a query'
+    labels_groups, scores_groups, queries_groups, doc_ids_groups = [], [], [], []
+    labels, scores, queries, doc_ids = [], [], [], []
+    doc_counter = 0
     for i in zip(label_list, score_list, query_docids_map):
         query = i[2][0]
+
         if (doc_counter > 0) and (last_query != query):
 
-            map_labels, map_bert_labels = get_stats(labels=labels, scores=scores)
-            query_counter +=1
-            map_labels_sum += map_labels
-            map_bert_labels_sum += map_bert_labels
-
             doc_counter = 0
-            labels = []
-            scores = []
+            labels_groups.append(labels)
+            scores_groups.append(scores)
+            queries_groups.append(queries)
+            doc_ids_groups.append(doc_ids)
+            labels, scores, queries, doc_ids = [], [], [], []
 
         labels.append(i[0])
         scores.append(i[1])
+        queries.append(query)
+        doc_ids.append(i[2][1])
+
         last_query = query
         doc_counter += 1
 
-    map_labels, map_bert_labels = get_stats(labels=labels, scores=scores)
-    map_labels_sum += map_labels
-    map_bert_labels_sum += map_bert_labels
-    query_counter += 1
+    labels_groups.append(labels)
+    scores_groups.append(scores)
+    queries_groups.append(queries)
+    doc_ids_groups.append(doc_ids)
 
-    return (map_labels_sum / query_counter), (map_bert_labels_sum / query_counter)
-
-
+    return labels_groups, scores_groups, queries_groups, doc_ids_groups
 
 
+# def split_bert_outputs(label_list, score_list, query_docids_map):
+#
+#     last_query = 'Not a query'
+#     doc_counter = 0
+#     labels = []
+#     scores = []
+#     query_counter = 0
+#     map_labels_sum = 0
+#     map_bert_labels_sum = 0
+#
+#     for i in zip(label_list, score_list, query_docids_map):
+#         query = i[2][0]
+#         if (doc_counter > 0) and (last_query != query):
+#
+#             map_labels, map_bert_labels = get_stats(labels=labels, scores=scores)
+#             query_counter += 1
+#             map_labels_sum += map_labels
+#             map_bert_labels_sum += map_bert_labels
+#
+#             doc_counter = 0
+#             labels = []
+#             scores = []
+#
+#         labels.append(i[0])
+#         scores.append(i[1])
+#         last_query = query
+#         doc_counter += 1
+#
+#     map_labels, map_bert_labels = get_stats(labels=labels, scores=scores)
+#     map_labels_sum += map_labels
+#     map_bert_labels_sum += map_bert_labels
+#     query_counter += 1
+#
+#     return (map_labels_sum / query_counter), (map_bert_labels_sum / query_counter)
 
+# scores
+# lables
+# query
+# doc_id
+
+
+def write_trec_run(score_list, query_docids_map):
+
+    last_query = 'Not a query'
+    doc_counter = 0
+    scores = []
+    doc_ids = []
+
+    for i in zip(score_list, query_docids_map):
+        query = i[0][0]
+        if (doc_counter > 0) and (last_query != query):
+
+            doc_counter = 0
+            scores = []
+            doc_ids = []
+
+        scores.append(i[0])
+        doc_ids.append(i[1][1])
+        last_query = query
+        doc_counter += 1
+
+    scores.append(i[0])
+    doc_ids.append(i[1][1])
+
+
+
+
+
+    # d = {i[0]: i[1] for i in zip(doc_ids, scores)}
+    # od = collections.OrderedDict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+    #
+    # run_file
+    # rank = 1
+    # for doc_id in od.keys():
+    #
+    #     output_line = " ".join((query, "Q0", str(doc_id), str(rank), str(od[doc_id]), "BERT"))
+    #     run_file.write(output_line + "\n")
+    #     rank += 1
+
+
+
+
+# possible_write = len(pred_list) // num_rank
+# while counter_written < possible_write:
+#
+#     start_idx = counter_written * num_rank
+#     end_idx = counter_written * num_rank + num_rank
+#
+#     scores = pred_list[start_idx:end_idx]
+#     query_docids = query_docids_map[start_idx:end_idx]
+#
+#     queries, doc_ids = zip(*query_docids)
+#     assert len(set(queries)) == 1, "Queries must be all the same. \n queries: {} \n doc_ids: {}".format(queries, doc_ids)
+#     assert len(query_docids) == len(scores) == num_rank, 'not correct dimensions'
+#     query = queries[0]
+#     print(query)
+#
+#     d = {i[0]:i[1] for i in zip(doc_ids, scores)}
+#     od = collections.OrderedDict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+#
+#     rank = 1
+#     for doc_id in od.keys():
+#
+#         output_line = " ".join((query, "Q0", str(doc_id), str(rank), str(od[doc_id]), "BERT"))
+#         run_file.write(output_line + "\n")
+#         rank += 1
+#
+#     counter_written += 1
 
 
 
