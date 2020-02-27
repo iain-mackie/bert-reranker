66ec59617d5ea35518dc54789dd5fa0e0de730c7
iain-mackie <i.mackie@me.com>
2020-02-27 12:44:56 +0000

    added map printing

--- metrics.py
+++ metrics.py
@@ -93,7 +93,7 @@ def get_metrics(labels_groups, scores_groups, rel_docs_groups):
         map_bert_sum += get_map(run=bert_labels)
         print('-------------')
         print(get_map(run=labels))
-        print('map: {0:.4f}'.format(labels))
+        print('map: {0:.4f}'.format(get_map(run=labels)))
 
         R_prec_labels_sum += get_R_prec(run=labels, R=R)
         R_prec_bert_sum += get_R_prec(run=bert_labels, R=R)
@@ -107,13 +107,13 @@ def get_metrics(labels_groups, scores_groups, rel_docs_groups):
         precision_20_bert_sum += get_precision(run=bert_labels, k=20)
         print('precision_20: {0:.4f}'.format(get_precision(run=labels, k=20)))
 
-        ndcg_20_labels_sum += get_ndcg(run=labels, k=20)
-        ndcg_20_bert_sum += get_ndcg(run=bert_labels, k=20)
-        print('ndcg_20: {0:.4f}'.format(get_ndcg(run=labels, k=20)))
-
         recall_40_labels_sum += get_recall(run=labels, k=40, R=R)
         recall_40_bert_sum += get_recall(run=bert_labels, k=40, R=R)
         print('recall_40: {0:.4f}'.format(get_recall(run=labels, k=40, R=R)))
+
+        ndcg_20_labels_sum += get_ndcg(run=labels, k=20)
+        ndcg_20_bert_sum += get_ndcg(run=bert_labels, k=20)
+        print('ndcg_20: {0:.4f}'.format(get_ndcg(run=labels, k=20)))
 
     num_queries = len(labels_groups)
 
@@ -124,9 +124,9 @@ def get_metrics(labels_groups, scores_groups, rel_docs_groups):
     ndcg_20_labels, ndcg_20_bert = ndcg_20_labels_sum / num_queries, ndcg_20_bert_sum / num_queries
     recall_40_labels, recall_40_bert = recall_40_labels_sum / num_queries, recall_40_bert_sum / num_queries
 
-    string_labels = ['map', 'R_prec', 'recip_rank', 'precision_20', 'ndcg_20', 'recall_40']
-    label_metrics = [map_labels, R_prec_labels, recip_rank_labels, precision_20_labels, ndcg_20_labels, recall_40_labels]
-    bert_metrics = [map_bert, R_prec_bert, recip_rank_bert, precision_20_bert, ndcg_20_bert, recall_40_bert]
+    string_labels = ['map', 'R_prec', 'recip_rank', 'precision_20', 'recall_40', 'ndcg_20']
+    label_metrics = [map_labels, R_prec_labels, recip_rank_labels, precision_20_labels, recall_40_labels, ndcg_20_labels]
+    bert_metrics = [map_bert, R_prec_bert, recip_rank_bert, precision_20_bert, recall_40_bert, ndcg_20_bert]
 
     return string_labels, label_metrics, bert_metrics
 
