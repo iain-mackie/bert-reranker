295cb8cda2dd70f119d658e58e12c7df72d1f1b2
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-02 19:16:47 +0000

    increased epochs

--- bert_models.py
+++ bert_models.py
@@ -377,7 +377,7 @@ if __name__ == "__main__":
     batch_size = 16*3
     pretrained_weights = 'bert-base-uncased'
     relevance_bert = nn.DataParallel(BertReRanker.from_pretrained(pretrained_weights))
-    epochs = 12
+    epochs = 15
     eps = 1e-8
     lr = 5e-5
     seed_val = 42
