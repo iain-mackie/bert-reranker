c78626d6c6027de00741d1eef1e211572be72ebf
iain-mackie <i.mackie@me.com>
2020-03-02 13:25:21 +0000

    chnage pasth to training data

--- bert_models.py
+++ bert_models.py
@@ -380,7 +380,7 @@ if __name__ == "__main__":
     validation_tensor = torch.load(dev_path)
     validation_dataloader = build_validation_data_loader(tensor=validation_tensor, batch_size=batch_size)
 
-    train_path = '/nfs/trec_car/data/bert_reranker_datasets/train_benchmarkY1_50.pt'
+    train_path = '/nfs/trec_car/data/bert_reranker_datasets/train_benchmarkY1_0.5.pt'
     print('loading train tensor: {}'.format(train_path))
     train_tensor = torch.load(train_path)
     train_dataloader = build_training_data_loader(tensor=train_tensor, batch_size=batch_size)
