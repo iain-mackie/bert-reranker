c819c0c3db7ced70782a0c6e975bbbc76ed48f25
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-06 15:08:24 +0000

    adding docker changes

--- docker/build_docker_python_run.sh
+++ docker/build_docker_python_run.sh
@@ -5,7 +5,7 @@ echo "Trying to build docker image from:" $DOCKER_PATH;
 
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
-  sudo docker build -t iainmackie/bert-reranker-python-run:v1 -f $DOCKER_PATH .
+  sudo docker build -t iainmackie/bert-reranker-python-run:v4 -f $DOCKER_PATH .
 
 else
   echo "Error - path to file not found:" $DOCKER_PATH;
